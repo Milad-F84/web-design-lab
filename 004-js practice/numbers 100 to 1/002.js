33b437329a841a619763e41d31816ab36001bfce
miladfarajollahi <mfarajollahifrontend@gmail.com>
2024-08-24 18:59:01 +0330

    4 practice complete

---
+++ 004-js practice/numbers 100 to 1/002.js
@@ -0,0 +1,5 @@
+function numbers(){
+    for(let i=100; i>=1; i--){
+        console.log(i);
+    }
+}
---
+++ 004-js practice/sum of numbers 1 to 100/004.js
@@ -0,0 +1,7 @@
+function sum(){
+    let total=0;
+    for(let i=1; i<=100; i++){
+        total = total+i;
+    }
+    console.log(total);
+}
