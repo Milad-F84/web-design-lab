9e4f8d49db0ea069f097a190504e4bc8581c70f3
miladfarajollahi <mfarajollahifrontend@gmail.com>
2024-08-25 20:11:45 +0330

    3 more practice complete

---
+++ 004-js practice/average 10 numbers/007.js
@@ -0,0 +1,14 @@
+function sum(num1,num2,num3,num4,num5,num6,num7,num8,num9,num10){
+    return (num1+num2+num3+num4+num5+num6+num7+num8+num9+num10)/10; 
+}
+num1=+prompt("enter num1");
+num2=+prompt("enter num2");
+num3=+prompt("enter num3");
+num4=+prompt("enter num4");
+num5=+prompt("enter num5");
+num6=+prompt("enter num6");
+num7=+prompt("enter num7");
+num8=+prompt("enter num8");
+num9=+prompt("enter num9");
+num10=+prompt("enter num10");
+console.log(sum(num1,num2,num3,num4,num5,num6,num7,num8,num9,num10));
---
+++ 004-js practice/sum of even numbers 1 to 100/005.js
@@ -0,0 +1,9 @@
+function sum(){
+    let total=0;
+    for(let i=1; i<100; i++){
+        if(i%2===0){
+            total = total + i;
+        }
+    }
+    console.log(total);
+}
