251e85f37f9cfac206240522d72d9208256270f2
miladfarajollahi <mfarajollahifrontend@gmail.com>
2024-09-13 14:03:56 +0330

    other part of practice complete

---
+++ 006- 3 practice of js/switch images/002.js
@@ -0,0 +1,9 @@
+const images = document.querySelectorAll("img");
+//console.log(images[4].getAttribute("src"));
+function imageSwitcher(){
+    let imgMother = images[0].getAttribute("src");
+    let item1 = images[1].getAttribute("src");
+    let item2 = images[2].getAttribute("src"); 
+    let item3 = images[3].getAttribute("src"); 
+    let item4 = images[4].getAttribute("src"); 
+}
