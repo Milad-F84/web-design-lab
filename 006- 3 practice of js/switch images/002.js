caec62622876a69a6dfe052dbf3ae5ed0349bf3a
miladfarajollahi <mfarajollahifrontend@gmail.com>
2024-09-15 19:40:59 +0330

    a part of practice complete

--- 006- 3 practice of js/switch images/002.js
+++ 006- 3 practice of js/switch images/002.js
@@ -7,8 +7,13 @@ function imageSwitcher(){
     let item2 = images[2].getAttribute("src"); 
     let item3 = images[3].getAttribute("src"); 
     let item4 = images[4].getAttribute("src"); 
-    images[0].setAttribute("src", item1);
-    images[0].setAttribute("src", item2);
-    images[0].setAttribute("src", item3);
-    images[0].setAttribute("src", item4);
+    if(document.getElementById('img1').clicked == true){
+        images[0].setAttribute("src", item1);
+    }else if(document.getElementById('img2').clicked == true){
+        images[0].setAttribute("src", item2);
+    }else if(document.getElementById('img3').clicked == true){
+        images[0].setAttribute("src", item3);
+    }else if(document.getElementById('img4').clicked == true){
+        images[0].setAttribute("src", item4);
+    }
 }
