05ab6a1da6c69012e9c55d03c45e3a0e42dde71c
miladfarajollahi <mfarajollahifrontend@gmail.com>
2024-09-28 18:05:07 +0330

    html and part of js complete

---
+++ 007-todo app/001.js
@@ -0,0 +1,19 @@
+//DOM NODES
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+const root = document.getElementById("root");
+let TODOS = [
+    {
+        id: 0,
+        Text: "football",
+        isDone: false
+    },
+    {
+        id: 1,
+        Text: "volleyball",
+        isDone: false
+    },
+]
+
+
+//FUNCTIONS
